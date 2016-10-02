3e1bffcd8e85b614605a94f140571d8240b0923f
François Jacob <papamaker.fr@gmail.com>
2016-10-02 09:06:35 +0200

    clean boot loader directory

--- bootloader/src/obj/bootloader.c.d
+++
@@ -1,2 +0,0 @@
-obj/./bootloader.c.o: bootloader.c boot.h prog_flash.h pin_defs.h \
-  stk500.h mmc_fat.h
---
+++ bootloader/src/obj/kitcoboot.c.d
@@ -0,0 +1,2 @@
+obj/./kitcoboot.c.o: kitcoboot.c boot.h prog_flash.h pin_defs.h stk500.h \
+  mmc_fat.h
