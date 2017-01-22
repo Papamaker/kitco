853984066cc9036d5fade78939f4f05027fbc4ea
François Jacob <papamaker.fr@gmail.com>
2017-01-22 16:19:51 +0100

    ajout de l'étape 2 du codage de Sokoban

--- lib/src/sokoban/sokobanEtape2/sokobanEtape2.ino
+++ lib/src/sokoban/sokobanEtape2/sokobanEtape2.ino
@@ -6,6 +6,10 @@
 #define NB_LIGNES_NIVEAUX 8
 #define NB_COLONNES_NIVEAUX 4
 
+// la position initiale du joueur (dépend du niveau)
+int ligneJoueur   = 1;
+int colonneJoueur = 1;
+
 char niveau[NB_LIGNES_NIVEAUX][NB_COLONNES_NIVEAUX] = 
 { {'M','M','M','M'},
   {'M','@','.','M'},
@@ -93,12 +97,125 @@ void afficheGraphismes() {
     rafraichirEcran();
 }
 
+//
+// LES CONTROLES
+//
+int relacheTouche = 0;
+
+#define HAUT   1
+#define BAS    2
+#define DROITE 3
+#define GAUCHE 4 
+
+// fonction ligneSuivante
+int ls(int direction, int nb) {
+  switch (direction) {
+    case HAUT:
+      return ligneJoueur-nb;
+      break;
+    case BAS:
+      return ligneJoueur+nb;
+      break;
+    default:
+      return ligneJoueur;
+      break;
+  }
+}
+
+// fonction colonneSuivante
+int cs(int direction, int nb) {
+  switch (direction) {
+    case DROITE:
+      return colonneJoueur + nb;
+      break;
+    case GAUCHE:
+      return colonneJoueur -nb;
+      break;
+    default:
+      return colonneJoueur;
+      break;
+  }
+}
+
+// racourci case suivante:
+#define SUIVANTE(DIR,NB) niveau[ls(DIR,NB)][cs(DIR,NB)]
+
+void deplace(int dir) {
+
+  // cas 1
+  if (SUIVANTE(dir,1) == 'M') {
+    return;
+  }
+
+  //cas 2
+  if ( (SUIVANTE(dir,1) == '$' || SUIVANTE(dir,1) == '*') &&
+       (SUIVANTE(dir,2) == '$' || SUIVANTE(dir,2) == '*')){
+   return;
+  }
+
+  // cas 3
+  if ( (SUIVANTE(dir,1) == '$' || SUIVANTE(dir,1) == '*') &&
+       (SUIVANTE(dir,2) == 'M')) {
+    return;
+  }
+
+  // à ce point du programme, on sait que le déplacement est autorisé  
+  // Si la case du dessus contient une caisse on la déplace
+  if (SUIVANTE(dir,1) == '$' || SUIVANTE(dir,1) == '*') {
+    if (SUIVANTE(dir,2) == '.') {
+      SUIVANTE(dir,2) = '*'; } else {
+      SUIVANTE(dir,2) = '$';
+    }
+    if (SUIVANTE(dir,1) == '$') {
+      SUIVANTE(dir,1) = ' '; } else {
+      SUIVANTE(dir,1) = '.';
+    }
+  }
+
+  // On déplace le joueur
+  if (SUIVANTE(dir,1) == '.') {
+   SUIVANTE(dir,1) = '+'; } else {
+   SUIVANTE(dir,1) = '@'; 
+  }
+
+  // Le joueur n'est plus à la cas précédente
+  if (niveau[ligneJoueur][colonneJoueur] == '+') {
+    niveau[ligneJoueur][colonneJoueur] = '.'; } else {
+    niveau[ligneJoueur][colonneJoueur] = ' '; 
+  }
+
+  ligneJoueur = ls(dir,1);
+  colonneJoueur = cs(dir,1);
+}
+
+void surveilleTouches() {
+  if (toucheHaut() && relacheTouche) {
+    deplace(HAUT);
+  }
+  if (toucheBas() && relacheTouche) {
+    deplace(BAS);
+  }
+  if (toucheDroite() && relacheTouche) {
+    deplace(DROITE);
+  }
+  if (toucheGauche() && relacheTouche) {
+    deplace(GAUCHE);
+  }
+  if (touche()) {
+    relacheTouche = 0;
+  } else {
+    relacheTouche = 1;
+  }
+}
+
+
+
 // Loop est la boucle principale, va se lancer en boucle après Setup
 void loop() {
 
   afficheGraphismes();
   // joueBruitages();
-  // surveilleTouches();
+   surveilleTouches();
   // logiqueJeu();
 
 }
